3acb5c5c10fd4e3ff3da2bbdc2d56e84058468e8
Althaf-Budiman <budimanaltaf687@gmail.com>
2024-05-09 13:12:57 +0700

    projects item rendered in projects page

--- script.js
+++ script.js
@@ -1,3 +1,75 @@
+const projects = [
+    {
+        img: 'forum-web.png',
+        name: 'Forum',
+        desc: 'a forum website.',
+        tags: ['tailwind', 'laravel', 'livewire']
+    },
+    {
+        img: 'museumJawa-web.png',
+        name: 'MuseumJawa',
+        desc: 'a java culture showcase website.',
+        tags: ['tailwind', 'react']
+    },
+    {
+        img: 'atopClean-web.png',
+        name: 'Atop Clean',
+        desc: 'a laundry management website.',
+        tags: ['tailwind', 'laravel', 'inertia', 'react']
+    },
+    {
+        img: 'sta-web.png',
+        name: 'Super Tasko Aria Company Web',
+        desc: 'an official Super Tasko Aria company website.',
+        tags: ['tailwind', 'laravel', 'inertia', 'react']
+    },
+    {
+        img: 'itFest-web.png',
+        name: 'IDN IT FEST 2023 Website',
+        desc: 'a laundry management website.',
+        tags: ['tailwind']
+    },
+    {
+        img: 'dishRecipe-api.png',
+        name: 'Dish Recipe API',
+        desc: 'api for dish recipe sharing website.',
+        tags: ['laravel']
+    },
+]
+
+window.addEventListener('load', () => {
+    const container = document.querySelector('.projects-container')
+
+    projects.forEach(project => {
+        const projectItem = document.createElement('div')
+        projectItem.classList.add('project-item')
+
+        const itemContent = `
+            <div class="project-item fly-hover">
+                <!-- img -->
+                <div class="project-item-img">
+                    <img src="assets/img/${project.img}" />
+                </div>
+
+                <!-- body -->
+                <div class="project-item-body">
+                    <div>
+                        <h2 class="font-semibold">${project.name}</h2>
+                        <p class="font-light">${project.desc}</p>
+                    </div>
+                    <div class="tags">
+                        ${project.tags.map(tag => `<div class="tag ${tag}"><span style="text-transform: capitalize;">${tag}</span></div>`).join(' ')}
+                    </div>
+                </div>
+            </div>
+        `
+
+        projectItem.innerHTML = itemContent
+        container.appendChild(projectItem)
+    })
+
+})
+
 function changeTab(tabName) {
     const tabs = document.querySelectorAll('.tab')
     const tabContents = document.querySelectorAll('.tab-content')
